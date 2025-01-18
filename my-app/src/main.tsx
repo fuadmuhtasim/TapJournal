4b52ac3fe882790d8925ed442cc1bef20c0247b6
fuadmuhtasim <jeetfuad@gmail.com>
2025-01-18 04:20:19 -0600

    Removed Reference to openai api

--- my-app/src/main.tsx
+++ my-app/src/main.tsx
@@ -3,8 +3,6 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 
 import Page from './app/dashboard/page-custom.tsx'
-import GPTRequest from './components/testopenai.tsx'
-
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
